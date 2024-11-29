395d5a292572b1ef93772178c0f0b81ec49da866
oguzhancirkin <oguzhancirkin@OGUZHANCIRKIN>
2024-11-29 14:33:26 +0300

    DatabaseCrud

--- 09-DatabaseProject/Program.cs
+++ 09-DatabaseProject/Program.cs
@@ -14,40 +14,39 @@ namespace _09_DatabaseProject
         {
             //Ado.net
 
-            Console.WriteLine("***** C# Veri Tabanlı Ürün - Kategori Bilgi Sisteni *****");
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine("***** C# Veri Tabanlı Ürün - Kategori Bilgi Sisteni *****");
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            string tableNumber;
+            //string tableNumber;
 
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("1-Kategoriler");
-            Console.WriteLine("2-Ürünler");
-            Console.WriteLine("3-Siparişler");
-            Console.WriteLine("4-Çıkış Yap");
-            Console.Write("Lütfen getirmek istediginiz tablo numarasını giriniz: ");
-            tableNumber = Console.ReadLine();
-            Console.WriteLine("----------------------------------------------------------");
+            //Console.WriteLine("----------------------------------------------------------");
+            //Console.WriteLine("1-Kategoriler");
+            //Console.WriteLine("2-Ürünler");
+            //Console.WriteLine("3-Siparişler");
+            //Console.WriteLine("4-Çıkış Yap");
+            //Console.Write("Lütfen getirmek istediginiz tablo numarasını giriniz: ");
+            //tableNumber = Console.ReadLine();
+            //Console.WriteLine("----------------------------------------------------------");
 
-            SqlConnection connection = new SqlConnection("Data Source = OGUZHANCIRKIN\\SQLEXPRESS; initial Catalog = EgitimKampiDb; integrated security = true");
-            connection.Open();
-            SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
-            SqlDataAdapter adapter = new SqlDataAdapter(command);
-            DataTable dataTable = new DataTable();
-            adapter.Fill(dataTable);
-            connection.Close();
+            //SqlConnection connection = new SqlConnection("Data Source = OGUZHANCIRKIN\\SQLEXPRESS; initial Catalog = EgitimKampiDb; integrated security = true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+            //connection.Close();
 
-            foreach (DataRow row in dataTable.Rows)
-            {
-                foreach (var item in row.ItemArray)
-                {
-                    Console.WriteLine(item.ToString());
-                }
-                Console.WriteLine();
-            }
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    foreach (var item in row.ItemArray)
+            //    {
+            //        Console.WriteLine(item.ToString());
+            //    }
+            //    Console.WriteLine();
+            //}
 
-
-            Console.Read();
+            //Console.Read();
 
 
         }
